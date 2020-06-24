eba946e6242e79aef6b4cf56225a34d96f3fc306
azamat.boranbaev <azaza7087@gmail.com>
2020-06-24 11:41:40 +0600

    The new version without bags

--- Test.py
+++ Test.py
@@ -34,22 +34,34 @@ def reg():
         for value in sql.execute("SELECT * FROM users"):
             print(value[0])
 
+
+def delete_db():
+    sql.execute(f'DELETE FROM users WHERE login = "{user_login}"')
+    db.commit()
+
+    print("Account is deleted")
+
 def casino():
+    global user_login
     user_login = input("Log in: ")
     number = randint(1, 2)
+
+    for i in sql.execute(f"SELECT cash FROM users WHERE login='{user_login}'"):
+        balance = i[0]
 
     sql.execute(f'SELECT login FROM users WHERE login="{user_login}"')
     if sql.fetchone() is None:
         print("You are not registrated")
         answer = input("Do you want to registrate? Y/N: ")
-        if answer == 'Y':
+        if answer == 'Y' or answer == 'y':
             reg()
         else:
             print("Ok")
     else:
         user_password = input("Enter password: ")#
-        sql.execute(f"SELECT password,login FROM users WHERE login='{user_login}', password = '{user_password}'")#\
-        if sql.fetchone() is None:
+
+        if sql.execute(f"SELECT login FROM users WHERE password = '{user_password}'")!= sql.execute(f"SELECT login FROM users WHERE login= '{user_login}'"):
+            print()
             print("Not correct")
         else:
             print("Playing game...")
@@ -60,9 +72,16 @@ def casino():
             if number == 1:
                 prise_money = randint(0,100)
                 print("You won {} $!".format(prise_money))
-                sql.execute(f'UPDATE users SET cash = {prise_money} WHERE login = "{user_login}"')
+                sql.execute(f'UPDATE users SET cash = {prise_money + balance} WHERE login = "{user_login}"')
                 db.commit()
             else:
                 print("You lose!")
+
+                answer = input("Do you want to delete your account? Y/N: ")
+                if answer == "Y":
+                    delete_db()
+                else:
+                    print("Ok")
+
 
 casino()
